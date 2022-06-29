923a4eba4ff3d3223b0d744588d0757d956fad1a
Baltazar <balti.kpo@gmail.com>
2022-06-29 14:54:57 -0300

    Changed env variables

--- firebase/admin/admin.js
+++ firebase/admin/admin.js
@@ -1,8 +1,19 @@
 const admin = require("firebase-admin")
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CONFIG)
-const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-serviceAccount.private_key = privateKey
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key.replace(/\\n/g, "\n"),
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+}
+
+console.log(serviceAccount)
 
 if (!admin.apps.length) {
   admin.initializeApp({
